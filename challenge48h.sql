5d4083058952de31f0d24dc83a50e700aab3ca26
KillianM92 <killian.moutinard@ynov.com>
2021-02-23 16:40:24 +0100

    final

--- challenge48h.sql
+++ challenge48h.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `challenge48h`
+-- Base de données : `heroku_51829f5bc18443e`
 --
 
 -- --------------------------------------------------------
--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'challenge48h' );
+define( 'DB_NAME', 'heroku_51829f5bc18443e' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'b23dc241378d2e' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '5d099814' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'eu-cdbr-west-03.cleardb.net' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8' );
